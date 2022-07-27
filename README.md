0d7b9edb4170ed6ba4129127f14dea5571e6f1a4
caseBread <kgu0515@khu.ac.kr>
2022-07-28 02:11:11 +0900

    [Fix] testCase

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 - [x] boostset class 구현
 - [x] countSet class 구현
-- [ ] 각각 메쏘드 -> 함수형 프로그래밍으로 개선
+- [x] 각각 메쏘드 -> 함수형 프로그래밍으로 개선
     - [x] sum 합집합 구현
     - [x] complement 구현
     - [x] intersect 구현
     - [x] append 구현
     - [x] remove 구현
+    - [x] sum 구현
+    - [x] complement 구현
+    - [x] intersect 구현
+    - [x] resultAll 구현
+
+- [x] 각 기능을 확인하기 위한 상위 프로그램 구현
 
 # 학습메모
 
--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const log = console.log;
 const BS = new BoostSet([1,2,3]);
 const otherBS = new BoostSet([1,3]);
 
-log(BS.sum(otherBS));
-log(BS.complement(otherBS));
-log(BS.intersect(otherBS));
-log(BS.resultAll());
+log("A 집합 :",BS.resultAll());
+log("B 집합 :",otherBS.resultAll());
+log("합집합 :",BS.sum(otherBS));
+log("여집합 :",BS.complement(otherBS));
+log("교집합 :",BS.intersect(otherBS));
 
 
 const CS = new CountSet({1:2,2:2,3:2});
 const otherCS = new CountSet({1:1,3:3});
 
-log(CS.sum(otherCS));
-log(CS.complement(otherCS));
-log(CS.intersect(otherCS));
-
+log("A count집합 :",CS.resultAll());
+log("B count집합 :",otherCS.resultAll());
+log("count 요소 더하기 :",CS.append(3));
+log("count 요소 빼기 :",CS.remove(3));
+log("count 합집합 :",CS.sum(otherCS));
+log("count 여집합 :",CS.complement(otherCS));
+log("count 교집합 :",CS.intersect(otherCS));
