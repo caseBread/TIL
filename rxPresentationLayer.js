44eef34048a7b7abe861d7e6db1446ed00c1ed3e
caseBread <kgu0515@khu.ac.kr>
2022-08-04 03:13:18 +0900

    [Add] FileWrite

--- rxPresentationLayer.js
+++ rxPresentationLayer.js
@@ -1,12 +1,13 @@
 const { rxApplication } = require("./rxApplicationLayer");
-
+const fs = require("fs");
 const log = console.log;
 
 const rxPresentation = (message) => {
     const newMessage = [ ...message ]
     newMessage[5] = Buffer.from(newMessage[5], "base64").toString('utf8')
+    fs.writeFileSync("./attachment.file", newMessage[5]);
     log("================= 수신 표현계층 ======================")
-    log(newMessage[5]);
+    log("디코딩 데이터 파일 : attachment.file")
     rxApplication(newMessage)
 }
 
