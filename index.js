b4efa4e4d3d13b1336908b6b11aba7dca020579a
caseBread <kgu0515@khu.ac.kr>
2022-08-04 03:18:01 +0900

    [Fix] input

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 const readline = require("readline");
 const { application } = require("./txApplicationLayer");
 const log = console.log;
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 let cnt = 4;
 let [ from, to, title, file ] = [
-    "jk@boostcamp.connect.or.kr", 
-    "camper@boostcamp.connect.or.kr", 
-    "Hello World", 
-    "Hello BoostCamper,\r\n\tThis message written by JK." 
+    "","","",""
 ];
-// log("From주소를 입력해 주세요");
-// rl.on('line', (line) => {
-//     if (cnt=== 4) {
-//         from = line;
-//         log("To주소를 입력해 주세요");
-//     } else if (cnt=== 3) {
-//         to = line;
-//         log("Title을 입력해 주세요");
-//     } else if (cnt=== 2) {
-//         title = line;
-//         log("첨부파일을 입력해 주세요");
-//     } else {
-//         file = line;
-//         rl.close();
-//     }
-//     cnt--;
-// })
+log("From주소를 입력해 주세요");
+rl.on('line', (line) => {
+    if (cnt=== 4) {
+        from = line;
+        log("To주소를 입력해 주세요");
+    } else if (cnt=== 3) {
+        to = line;
+        log("Title을 입력해 주세요");
+    } else if (cnt=== 2) {
+        title = line;
+        log("첨부파일을 입력해 주세요");
+    } else {
+        file = line;
+        rl.close();
+    }
+    cnt--;
+})
 
-// rl.on('close', () => {
-//     main();
-//     process.exit();
-// })
+rl.on('close', () => {
+    main();
+    process.exit();
+})
 
 const main = () => {
     application(from,to,title,file);
 
 }
 
-main();
