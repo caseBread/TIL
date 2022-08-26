d11dd2461da7196deed21f592d20e9e28e95fad1
caseBread <kgu0515@gmail.com>
2022-08-26 16:06:21 +0900

    ~6.3

--- react/nomad-react/src/App.js
+++ react/nomad-react/src/App.js
@@ -3,19 +3,37 @@ import "./App.css";
 import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
-function App() {
+
+function Apps() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("i run all time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("i run only one");
+    console.log("CALL THE API...");
   }, []);
+  useEffect(() => {
+    console.log(`I run when 'keyword' changes.`);
+    // if (keyword !== "" && keyword.length > 5) {
+    //   console.log("SEARCH FOR", keyword);
+    // }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log(`I run when 'counter' changes.`);
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h2>{counter}</h2>
       <button onClick={onClick}>plus one</button>
     </div>
   );
 }
 
-export default App;
+export default Apps;
